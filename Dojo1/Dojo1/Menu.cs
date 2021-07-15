dd1e09a7aee6c331d6282e88c8efb1ebbc6cbf65
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-15 11:02:44 -0300

    Listagens de Produto e Categoria

--- Dojo1/Dojo1/Menu.cs
+++ Dojo1/Dojo1/Menu.cs
@@ -34,54 +34,79 @@ namespace Dojo1
         {
             Console.WriteLine("\n 1 - Cadastrar Produto \n 2 - Cadastro Categoria \n 3 - Listar Produtos \n 4 - Listar Categorias \n 0 - Sair");
         }
-        
+        public void Voltar()
+        {
+            Console.WriteLine("Digite qualquer tecla para voltar para o menu principal!");
+            Console.ReadLine();
+        }
         public void Escolha()
         {
-            Console.Write("Escolha uma opcao: ");
-            int opcao = Convert.ToInt32(Console.ReadLine());
-            switch (opcao)
+            do
             {
-                case 0:
-                    //Sair
+                Console.Write("Escolha uma opcao: ");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 0:
+                        //Sair
 
-                    break;
-                case 1:
-                    //Cadastrar produto
-                    do
-                    {
-                        Produto produto = new Produto();
-                        produto.CadastroProduto();
-                        produtos.Add(produto);
-                    } while (Sn());
+                        break;
+                    case 1:
+                        //Cadastrar produto
+                        do
+                        {
+                            Produto produto = new Produto();
+                            produto.CadastroProduto();
+                            produtos.Add(produto);
+                        } while (Sn(2));
                     
 
-                    break;
-                case 2:
-                    //Cadastrar categoria
-                    do
-                    {
-                        Categoria categoria = new Categoria();
-                        categoria.Cadastro();
-                        categorias.Add(categoria);
-                    } while (Sn());
-                    break;
-                case 3:
-                    //Listar Produto
-
-                    break;
-                case 4:
-                    //Listar Categoria
-                    break;
-            }
+                        break;
+                    case 2:
+                        //Cadastrar categoria
+                        do
+                        {
+                            Categoria categoria = new Categoria();
+                            categoria.Cadastro();
+                            categorias.Add(categoria);
+                        } while (Sn(2));
+                        break;
+                    case 3:
+                        //Listar Produto
+                        foreach (var item in produtos)
+                        {
+                            Console.WriteLine($"\nID: {item.Id}\nNome: {item.Nome}\n Valor: {item.Valor}");
+                        }
+                        Voltar();
+                        break;
+                    case 4:
+                        //Listar Categoria
+                        foreach (var item in categorias)
+                        {
+                            Console.WriteLine($"\nID: {item.Id}\nNome: {item.Nome}\n Descrição: {item.Descricao}");
+                        }
+                        Voltar();
+                        break;
+                }
+                Console.Clear();
+            } while (Sn(1));
 
         }
-        public bool Sn()
+        public bool Sn(int opcao)
         {
             bool resposta;
             string resp;
             do
             {
-                Console.WriteLine("Deseja cadastrar um novo item?");
+                if (opcao == 1)
+                {
+                    Opcoes();
+                    Escolha();
+                }
+                else
+                {
+                    Console.WriteLine("Deseja cadastrar um novo item?");
+                }
                 resp = Console.ReadLine().Trim().ToLower();
                 switch (resp)
                 {
