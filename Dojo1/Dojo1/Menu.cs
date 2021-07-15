0c2910cf4eddbe3218e66e06e2fbca1285ace3b8
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-15 09:02:33 -0300

    ROdape e Opcoes

--- Dojo1/Dojo1/Menu.cs
+++ Dojo1/Dojo1/Menu.cs
@@ -11,6 +11,8 @@ namespace Dojo1
         public void MontaMenu()
         {
             Cabecalho();
+            Opcoes();
+            Rodape();
         }
 
         public void Cabecalho()
@@ -18,6 +20,15 @@ namespace Dojo1
             Console.WriteLine("******************** DOJO LABS ********************");
             Console.WriteLine("******************** Bem-vindo ********************");
 
+        }
+
+        public void Rodape()
+        {
+            Console.WriteLine("---------------------------------------------------");
+        }
+        public void Opcoes()
+        {
+            Console.WriteLine("\n 1 - Cadastrar Produto \n 2 - Cadastro Categoria \n 3 - Listar Produtos \n 4 - Listar Categorias \n 0 - Sair");
         }
     }
 }
